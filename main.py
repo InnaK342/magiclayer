eeaf5e7859e0d58f78b1a3b261f184bd99e4cf8e
InnaK342 <65236928+InnaK342@users.noreply.github.com>
2022-10-13 13:00:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+import random
+
+TOKEN = '5686345550:AAGUyClcupHPWVomp7o_iGY5a1CLGOhpPZo'
+bot = telebot.TeleBot(TOKEN)
+answers = ['Так', 'Ні', 'Не можу відповісти...', 'Запитай пізніше', 'Без сумніву', 'Ніколи в житті', 'На жаль, ні', 'Все в твоїх руках', 'Звичайно', 'Мені потрібний відпочинок, запитай пізніше', 'Однозначно так', 'Хороші перспективи', 'Краще не розповідати', 'Дуже сумнівно', 'Перспективи не дуже хороші', 'Навіть не думай', 'Зараз не можна передбачити']
+
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Привіт, напиши запитання, яке тебе цікавить:)')
+
+
+@bot.message_handler(commands=["help"])
+def about(m, res=False):
+    bot.send_message(m.chat.id, 'Це магічна куля, яка розроблена для передбачення майбутнього.')
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, random.choice(answers))
+
+
+bot.polling(none_stop=True, interval=0)
